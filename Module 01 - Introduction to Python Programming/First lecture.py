97e621d49f19fc2c35e091c7bae88c791e8beaa3
Hafiz Salman <hafizsabonline27@gmail.com>
2025-07-24 14:46:52 +0500

    Add files via upload

---
+++ Module 01 - Introduction to Python Programming/First lecture.py
@@ -0,0 +1,34 @@
+#print Fucntion
+
+print("Learning Python")
+print("Hafiz Salman")
+print(10)
+
+name = "Hafiz"
+Name = "Salman"
+
+print(id(name))
+print(id(Name))
+
+
+#Declaring Variable and Assigning Values
+
+a = 40
+b = 90
+c = 100
+
+print(a,b,c, sep=" ")
+
+#Print Single and Multiple Variables in Python
+
+a = 5 
+print (a)
+print((a))
+
+#user input
+
+name = input("Enter your name : ")
+print ("welcome", name)
+age = int(input("Enter your age : "))
+print(age)
+number = int(input("Enter your number : "))
